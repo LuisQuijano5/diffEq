92ed4cc2f9c900f1a70dfd72d1b151d3aadef9da
Luis Quijano <luis5.quijano5@gmail.com>
2024-05-25 18:38:48 -0600

    setting the graphics in our function

--- Ours.py
+++ Ours.py
@@ -1,13 +1,31 @@
 import numpy as np
+from matplotlib import pyplot as plt
+from RK4 import run
 
-# Our parameters
+# Our parameters, you can modify them to see different results
 P = 2 / 100
 Q = 2 / 100
 U = 5.7
 
 
 def our_function(t, y):
-    dy = [U * y[1] - y[2] - 0.1 * y[0],
-          y[0] + (y[1] * P * (1 - y[0])) - 0.05 * y[1],
-          (Q + y[2]) + y[1] * - 0.2 * y[2]]
+    dy = [U * y[1] - y[2],
+          y[0] + (y[1] * P) - 0.05 * y[1],
+          y[0] * y[1]]
     return np.array(dy)
+
+
+y0 = [-1, 28, 6]  # these are the initial values
+dt = 0.01  # dt and T will ultimately set the number of iterations
+T = 15
+Y = [[], [], []]
+
+Y[0], Y[1], Y[2], min_values, max_values = run(y0, dt, T, our_function)
+#values
+print("X values: " + str(Y[0]))
+print("Y values: " + str(Y[1]))
+print("Z values: " + str(Y[2]))
+#draw
+ax = plt.figure().add_subplot(projection='3d')
+ax.plot(Y[0], Y[1], Y[2], 'b')
+plt.show()
